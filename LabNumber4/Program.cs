1287a8e6eb332e400a01b0ff95a0fd87a51e1630
Aleena Tucker <lena.tucker@outlook.com>
2016-10-27 15:20:15 -0400

    it got all whacked up after we did the unit testing

--- LabNumber4/Program.cs
+++ LabNumber4/Program.cs
@@ -10,6 +10,24 @@ namespace LabNumber4
     {
         static void Main(string[] args)
         {
+            int x;
+            long Fact;
+            Console.WriteLine("Enter an integer: ");
+            x = int.Parse(Console.ReadLine());
+            Fact = x;  //factorial equals the number we just parsed
+            Fact = getFact(x, Fact);
+            Console.ReadLine();
+
+        }
+
+        private static long getFact(int x, long Fact)
+        {
+            for (int i = x - 1; i >= 1; i--)  //for the numbers greater than one, decrement
+            {
+                Fact = Fact * i;  //multiply the factorial by the decremented value
+            }
+            Console.WriteLine("\n Factorial of Given Number is: " + Fact);
+            return Fact;
         }
     }
 }
---
+++ TestLab4/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestLab4
+{
+    public class Program
+    {
+        public static long calculateFactorial(int x)
+        {
+            return 0;
+        }
+        [TestClass]
+        public class UnitTest1
+        {
+            int x;
+            long Fact;
+            Console.WriteLine("Enter an integer: ");
+            x = int.Parse(Console.ReadLine());
+            Fact = x;  //factorial equals the number we just parsed
+            Fact = getFact(x, Fact);
+            Console.ReadLine();
+            [TestMethod]
+            private static long getFact(int x, long Fact)
+            {
+                for (int i = x - 1; i >= 1; i--)  //for the numbers greater than one, decrement
+                {
+                    Fact = Fact * i;  //multiply the factorial by the decremented value
+                }
+                Console.WriteLine("\n Factorial of Given Number is: " + Fact);
+                return Fact;
+            }
+        }
+    }
+}
